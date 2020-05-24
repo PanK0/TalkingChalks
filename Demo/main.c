19f48f1ae7b03d0f747a433c3a339c9c7559c6f0
Pank0 <panici.k@gmail.com>
2020-05-24 13:58:09 +0200

    test

--- Demo/main.c
+++ Demo/main.c
@@ -16,22 +16,59 @@
 
 semtech_loramac_t loramac;
 
+/*  #########################################
+    BEGIN STATIC INFORMATION
+    appeuis, deveuis ecc
+    NB : This section is only for testing purposes. Each device will have its own identity later.
+*/
+
+uint8_t dev_00_deveui[LORAMAC_DEVEUI_LEN];
+uint8_t dev_00_appeui[LORAMAC_APPEUI_LEN];
+uint8_t dev_00_appkey[LORAMAC_APPKEY_LEN];
+fmt_hex_bytes(dev_00_deveui, "00E826F416B86F3F");
+fmt_hex_bytes(dev_00_appeui, "70B3D57ED002F7A1");
+fmt_hex_bytes(dev_00_appkey,"D4608EA823673E4381F3328AECE139EF");
+
+uint8_t dev_01_deveui[LORAMAC_DEVEUI_LEN];
+uint8_t dev_01_appeui[LORAMAC_APPEUI_LEN];
+uint8_t dev_01_appkey[LORAMAC_APPKEY_LEN];
+fmt_hex_bytes(dev_01_deveui, "00BBBC8EC6FF6DD2");
+fmt_hex_bytes(dev_01_appeui, "70B3D57ED002F7A1");
+fmt_hex_bytes(dev_01_appkey,"86C9166C4540AC53B4EA29526D2C0306");
+
+uint8_t dev_02_deveui[LORAMAC_DEVEUI_LEN];
+uint8_t dev_02_appeui[LORAMAC_APPEUI_LEN];
+uint8_t dev_02_appkey[LORAMAC_APPKEY_LEN];
+fmt_hex_bytes(dev_02_deveui, "0077981F53BF8BD9");
+fmt_hex_bytes(dev_02_appeui, "70B3D57ED002F7A1");
+fmt_hex_bytes(dev_02_appkey,"685E563574362452A88F75074097E35E");
+
+uint8_t dr_standard = 5;
+uint8_t join_type_standard = LORAMAC_JOIN_OTAA;
+char devid[6];
+
+/*
+    END STATIC INFORMATION
+    #########################################
+*/
+
+
+
 /* Functions to emulate Profile With Brachelet
 *  Generating random values for the sensors simulation
 * */
 
-char levels[3][10] = {"Maria", "Hugo", "Ettore"};
-char ids[3][10] = {"ID00", "ID01", "ID02"};
+char profiles[3][10] = {"Maria", "Hugo", "Ettore"};
+char ids[3][6] = {"dev_00", "dev_01", "dev_02"};
 
-int get_random_value(int lower, int upper) {
+uint8_t get_random_value(uint8_t lower, uint8_t upper) {
     return ( rand() % (upper + 1 - lower) + lower);
 }
 
-int get_random_payload(char* payload) {
-    int profile = get_random_value(0, 2);
-    int id = get_random_value(0, 2);
+uint8_t get_random_payload(char* payload) {
+    uint8_t profile = get_random_value(0, 2);
 
-    sprintf(payload, "{\"dev_id\" : \"%s\", \"profile_id\": \"%s\"}", ids[id], levels[profile]);
+    sprintf(payload, "{\"dev_id\" : \"%s\", \"profile_id\": \"%s\"}", devid, profiles[profile]);
 
     return 0;
 }
@@ -73,13 +110,18 @@ static void _loramac_get_usage(void)
          "class|dr|adr|public|netid|tx_power|rx2_freq|rx2_dr>");
 }
 
-// Loop for sending data added at the else-if when argv[1] is equals to 'start', about line 477
 static int _cmd_loramac(int argc, char **argv)
 {
     if (argc < 2) {
         _loramac_usage();
         return 1;
     }
+
+    /*  #########################################
+        BEGIN LORAMAC GET
+        get: deveui, appeui, appkey, appskey, nwkskey, devaddr, class, dr, adr
+            public, netid, tx_power, rx2_freq, rx2_dr
+    */
 
     if (strcmp(argv[1], "get") == 0) {
         if (argc < 3) {
@@ -175,6 +217,17 @@ static int _cmd_loramac(int argc, char **argv)
             return 1;
         }
     }
+    /*
+        END LORAMAC GET
+        #########################################
+    */
+
+    /*  #########################################
+        BEGIN LORAMAC SET
+        set: deveui, appeui, appkey, appskey, nwkskey, devaddr, class [A, B, C], dr, adr
+            public [on, off], netid, tx_power, rx2_freq, rx2_dr
+    */
+
     else if (strcmp(argv[1], "set") == 0) {
         if (argc < 3) {
             _loramac_set_usage();
@@ -343,11 +396,43 @@ static int _cmd_loramac(int argc, char **argv)
             }
             semtech_loramac_set_rx2_dr(&loramac, dr);
         }
+        else if (strcmp("dev_00", argv[2]) == 0) {
+            semtech_loramac_set_deveui(&loramac, dev_00_deveui);
+            semtech_loramac_set_appeui(&loramac, dev_00_appeui);
+            semtech_loramac_set_appkey(&loramac, dev_00_appkey);
+            semtech_loramac_set_dr(&loramac, dr_standard);
+            strcpy(devid, "dev_00");
+        }
+        else if (strcmp("dev_01", argv[2]) == 0) {
+          semtech_loramac_set_deveui(&loramac, dev_01_deveui);
+          semtech_loramac_set_appeui(&loramac, dev_01_appeui);
+          semtech_loramac_set_appkey(&loramac, dev_01_appkey);
+          semtech_loramac_set_dr(&loramac, dr_standard);
+          strcpy(devid, "dev_01");
+        }
+        else if (strcmp("dev_02", argv[2]) == 0) {
+          semtech_loramac_set_deveui(&loramac, dev_02_deveui);
+          semtech_loramac_set_appeui(&loramac, dev_02_appeui);
+          semtech_loramac_set_appkey(&loramac, dev_02_appkey);
+          semtech_loramac_set_dr(&loramac, dr_standard);
+          strcpy(devid, "dev_02");
+        }
+
         else {
             _loramac_set_usage();
             return 1;
         }
     }
+
+    /*
+        END LORAMAC SET
+        #########################################
+    */
+
+    /*  #########################################
+        BEGIN LORAMAC JOIN
+        join : otaa, abp
+    */
     else if (strcmp(argv[1], "join") == 0) {
         if (argc < 3) {
             _loramac_join_usage();
@@ -387,6 +472,14 @@ static int _cmd_loramac(int argc, char **argv)
         }
         return 0;
     }
+    /*
+        END LORAMAC JOIN
+        #########################################
+    */
+
+    /*  #########################################
+        BEGIN LORAMAC TX
+    */
     else if (strcmp(argv[1], "tx") == 0) {
         if (argc < 3) {
             _loramac_tx_usage();
@@ -475,37 +568,21 @@ static int _cmd_loramac(int argc, char **argv)
 
         return 0;
     }
-   /* START LOOP DATA SENDING
-   *  Here we start sending random generated data after the command 'start'
-   */
+
+    /*
+        END LORAMAC TX
+        #########################################
+    */
+
+    /*  #########################################
+        BEGIN LORAMAC START
+    */
     else if (strcmp(argv[1], "start") == 0) {
 
         printf ("Starting . . .\n");
 
         uint8_t cnf = LORAMAC_DEFAULT_TX_MODE;  /* Default: confirmable */
         uint8_t port = LORAMAC_DEFAULT_TX_PORT; /* Default: 2 */
-        /* handle optional parameters */
-        if (argc > 3) {
-            if (strcmp(argv[3], "cnf") == 0) {
-                cnf = LORAMAC_TX_CNF;
-            }
-            else if (strcmp(argv[3], "uncnf") == 0) {
-                cnf = LORAMAC_TX_UNCNF;
-            }
-            else {
-                _loramac_tx_usage();
-                return 1;
-            }
-
-            if (argc > 4) {
-                port = atoi(argv[4]);
-                if (port == 0 || port >= 224) {
-                    printf("error: invalid port given '%d', "
-                           "port can only be between 1 and 223\n", port);
-                    return 1;
-                }
-            }
-        }
 
         semtech_loramac_set_tx_mode(&loramac, cnf);
         semtech_loramac_set_tx_port(&loramac, port);
@@ -582,11 +659,20 @@ static int _cmd_loramac(int argc, char **argv)
             puts("Message sent with success");
 
             // Maintain this value high in order to avoid duty cycle restrictions
-            xtimer_sleep(180);
+            int sleep = (int) get_random_value(40, 150);
+            printf ("Sleep for : %d", sleep);
+            xtimer_sleep(sleep);
         }
         return 0;
     }
-  /* END LOOP DATA SENDING  */
+    /*
+        END LORAMAC START
+        #########################################
+    */
+
+    /*  #########################################
+        BEGIN STUFFS
+    */
     else if (strcmp(argv[1], "link_check") == 0) {
         if (argc > 2) {
             _loramac_usage();
@@ -621,6 +707,11 @@ static int _cmd_loramac(int argc, char **argv)
 
     return 0;
 }
+
+/*
+    END STUFFS
+    #########################################
+*/
 
 static const shell_command_t shell_commands[] = {
     { "loramac", "control the loramac stack", _cmd_loramac },
